6fa8958c383b210b3f17c36b055c8fbc3e9b9b9f
iRB Air <irockbunny@outlook.com>
2017-06-05 17:42:47 +0800

    Update

--- assets/js/n-back.js
+++ assets/js/n-back.js
@@ -1,3 +1,28 @@
 // n-back.js
 
-var stimuli = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'];
+var stimuliPool = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'];
+
+function getStimuli(step, totalNum, targetNum) {
+    var stimuli = [];
+    if (step === 0) {
+        for (var i = 0; i < totalNum; i++) {
+            stimuli.push(_.sample(stimuliPool));
+        };
+    } else {
+        for (var i = 0; i < totalNum; i++) {
+            if (i < step) {
+                stimuli.push(_.sample(stimuliPool));
+            } else {
+                var target = _.nth(stimuli, -step);
+                if (_.random(1, totalNum - i) <= targetNum) {
+                    targetNum--;
+                    stimuli.push(target);
+                } else {
+                    stimuli.push(_.sample(_.difference(stimuliPool, [target])));
+                };
+            };
+            console.log(targetNum);
+        };
+    };
+    return stimuli;
+};
