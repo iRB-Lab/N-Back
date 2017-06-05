494931d98d97e68e07c6a3aaa0fa7a8464974bdd
iRB Air <irockbunny@outlook.com>
2017-06-05 19:31:12 +0800

    Update

--- assets/js/n-back.js
+++ assets/js/n-back.js
@@ -2,10 +2,7 @@
 
 var stimuliPool = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'];
 
-function getStimuli(step, totalNum, targetNum) {
-    if (!(_.isNumber(step) && _.isNumber(totalNum) && _.isNumber(targetNum) && step >= 0)) {
-        return;
-    };
+function getStimuli(step, totalNum=48, targetNum=16) {
     var stimuli = [];
     if (step === 0) {
         for (var i = 0; i < totalNum; i++) {
@@ -14,7 +11,7 @@ function getStimuli(step, totalNum, targetNum) {
                 'is_target': true
             });
         };
-    } else {
+    } else if (step > 0) {
         for (var i = 0; i < totalNum; i++) {
             if (i < step) {
                 stimuli.push({
@@ -41,8 +38,13 @@ function getStimuli(step, totalNum, targetNum) {
     return stimuli;
 };
 
-function getBlocks() {
-    var conditions = [0, 1, 2];
+function getBlocks(conditions=[0, 1, 2]) {
     var blocks = _.sampleSize(conditions, 2);
     return _.concat(_.shuffle(_.difference(conditions, [blocks[0]])), blocks, _.shuffle(_.difference(conditions, [blocks[1]])));
 };
+
+function getSession(conditions=[0, 1, 2], totalNum=48, targetNum=16) {
+    return _.map(getBlocks(conditions), function (step) {
+        return getStimuli(step, totalNum, targetNum);
+    });
+}
