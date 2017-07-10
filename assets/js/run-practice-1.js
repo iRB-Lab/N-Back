e5c260822ef5ea27aa2c27436578fd70727bc8a4
iROCKBUNNY <irockbunny@outlook.com>
2017-07-10 14:43:58 +0800

    Update

--- assets/js/run-practice-1.js
+++ assets/js/run-practice-1.js
@@ -1,25 +1,4 @@
 // customize-practice-1.js
 
 var blocks = generatePracticeBlocks(levels=[1]);
-loadCurrentBlock(blocks[currentBlockIndex]);
-setInterval(function () {
-    if (!currentBlockLoaded) {
-        loadCurrentBlock(blocks[currentBlockIndex]);
-    };
-    if (currentBlockRunning) {
-        tick();
-        if (currentStimulusIndex < blocks[currentBlockIndex].stimuli.length) {
-            if (elapsedTime === blocks[currentBlockIndex].stimuli[currentStimulusIndex].load_time) {
-                updateStimulusCache();
-                loadCurrentStimulus(blocks[currentBlockIndex].stimuli[currentStimulusIndex].stimulus);
-            };
-            if (elapsedTime === blocks[currentBlockIndex].stimuli[currentStimulusIndex].unload_time) {
-                unloadCurrentStimulus();
-                nextStimulus();
-            };
-        } else if (elapsedTime === blocks[currentBlockIndex].total_time) {
-            stopCurrentBlock();
-            loadRSME();
-        };
-    };
-}, taskOptions.granularity);
+runTask(blocks);
